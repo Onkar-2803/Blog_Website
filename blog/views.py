f1a280113dcd496f7b100545bfde91078ff44699
Onkar Rupesh Litake <60955023+Onkar-2803@users.noreply.github.com>
2020-11-23 23:02:23 +0530

    Initial Commit

---
+++ blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
